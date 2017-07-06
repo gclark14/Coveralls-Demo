610acb404a5ecfe7f0048c415a1f97a0ac6f19e5
Gabe Clark <unrivalled.dh@gmail.com>
2017-07-06 13:50:22 -0700

    Initial feature commit, contains a tested add function

---
+++ src/main/java/com/evision/Adder.java
@@ -0,0 +1,8 @@
+package com.evision;
+
+/**
+ * Created by gabe.clark on 7/6/17.
+ */
+public class Adder {
+    int add(int a, int b) { return a + b; }
+}
---
+++ src/main/java/com/evision/CoverallsdemoApplication.java
@@ -0,0 +1,12 @@
+package com.evision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoverallsdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoverallsdemoApplication.class, args);
+	}
+}
